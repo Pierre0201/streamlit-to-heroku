6f4546431e26d88f8ed3ac61bb558bdddcb48f85
Pierre0201 <108532397+Pierre0201@users.noreply.github.com>
2022-08-04 22:55:04 +0200

    Update fastapi_request.py

--- src/fastapi_request.py
+++ src/fastapi_request.py
@@ -4,24 +4,25 @@ Created on Fri Jul 29 00:40:59 2022
 
 @author: Pierre
 """
-import requests
-import pandas as pd
-
-path = 'https://raw.githubusercontent.com/Pierre0201/streamlit-to-heroku/main/src/ressources/'
-test_df = pd.read_csv(path+'submission_kernel02.csv')
-feats = [f for f in test_df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
-
 
 HOST = 'https://fastapi-clf-predict.herokuapp.com/'
 
 def get_prediction(id_credit):
-    """Gets the probability of default of a client on the API server.
-    Args : 
-    - id_client (int).
-    Returns :
-    - probability of default (float).
-    """
-    json_credit = test_df[feats].loc[test_df['SK_ID_CURR']==id_credit].to_json()
-    response = requests.get(HOST + '/prediction/', data=json_credit)
+    response = requests.get(HOST + 'predict?id_credit=' + id_credit)
     proba_default = eval(response.content)["probability"]
     return proba_default
+
+def minimum():
+    response = requests.get(HOST + 'minimum/')
+    minimum = eval(response.content)["min"]
+    return minimum
+
+def maximum():
+    response = requests.get(HOST + 'maximum/')
+    maximum = eval(response.content)["max"]
+    return maximum
+
+def median():
+    response = requests.get(HOST + 'median/')
+    median = eval(response.content)["median"]
+    return median
