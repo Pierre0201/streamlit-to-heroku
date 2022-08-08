0691b972443763d0c7bb85ba8a71846157e34b9f
nathalie <nathalie@data-terrae.fr>
2022-08-08 15:31:50 +0200

    :alembic: call prediction API with full credit data

--- src/fastapi_request.py
+++ src/fastapi_request.py
@@ -6,12 +6,21 @@ Created on Fri Jul 29 00:40:59 2022
 """
 
 import requests
+import pandas as pd
 
 HOST = 'https://fastapi-clf-predict.herokuapp.com/'
 
-def get_prediction(id_credit):
-    response = requests.get(HOST + 'predict?id_credit=' + id_credit)
-    proba_default = eval(response.content)["probability"]
+
+
+def get_prediction(credit_data: pd.Series):
+    """
+    Args:
+        credit_data (pd.Series): credit data for one customer
+
+    Returns: (float) probability of credit default, ranging from 0 to 1.
+    """
+    response = requests.post(HOST + 'prediction', data=credit_data.to_json())
+    proba_default = response.json()["probability"]
     return proba_default
 
 def minimum():
